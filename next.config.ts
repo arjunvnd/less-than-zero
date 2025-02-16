dca2ea7be9cd4e5129cbe9dcaea0406a15c6eb66
Arjun V <arjunvnd@gmail.com>
2025-02-16 16:00:45 +0530

    update next config

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,5 @@
-const isProd = process.env.NODE_ENV === "production";
 const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    unoptimized: true, // Disable default image optimization
-  },
-  assetPrefix: isProd ? "/less-than-zero/" : "",
-  basePath: isProd ? "/less-than-zero" : "",
+  basePath: "/less-than-zero",
   output: "export",
 };
 
