b9c905f96e302d81bd1073006ca9c02fb69506a8
Arjun V <arjunvnd@gmail.com>
2025-03-08 18:27:36 +0530

    update styles

--- app/(dashboard)/admin/page.tsx
+++ app/(dashboard)/admin/page.tsx
@@ -1,11 +1,52 @@
 import AssignVendorTable from "@/app/components/tables/assign-vendor-table";
-import { Grid2 } from "@mui/material";
+import { Grid2, Stack, TextField } from "@mui/material";
 
 export default function AdminLandingPage() {
   return (
     <>
-      <Grid2 container sx={{ p: 4 }}>
-        <AssignVendorTable />
+      <Grid2 container>
+        <Grid2 mb={3} size={{ xs: 12 }}>
+          <Grid2
+            container
+            spacing={{
+              md: 2,
+            }}
+          >
+            <Grid2 size={{ xs: 12, md: 6 }}>
+              <TextField
+                name="search"
+                placeholder="Search"
+                label="Search"
+                type="search"
+                fullWidth
+                variant="filled"
+              />
+            </Grid2>
+            <Grid2 size={{ xs: 12, md: 6 }}>
+              <Stack direction="row" spacing={2}>
+                <TextField
+                  name="budget"
+                  placeholder="Filter by budget"
+                  label="Filter by budget"
+                  fullWidth
+                  variant="filled"
+                  select
+                />
+                <TextField
+                  name="date"
+                  placeholder="Filter by date"
+                  label="Filter by date"
+                  fullWidth
+                  variant="filled"
+                  select
+                />
+              </Stack>
+            </Grid2>
+          </Grid2>
+        </Grid2>
+        <Grid2 size={{ xs: 12 }}>
+          <AssignVendorTable />
+        </Grid2>
       </Grid2>
     </>
   );
